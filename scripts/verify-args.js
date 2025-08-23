0dfaabd719b2d0b1d95db150012918a2bda87145
DevDiner <0xirwin@gmail.com>
2025-08-23 22:55:13 +0800

    added verify-args.js verifying script for Sepolia

---
+++ scripts/verify-args.js
@@ -0,0 +1,8 @@
+module.exports = [
+  "0x4655cf84f14bc71e44d7629cd04a37209a8212fdaa55244bd8f43432efa7898b", // merkleRoot
+  [
+    "0xd439c776E10119a42B19838614F772FeB5f42591",
+    "0xd439c776E10119a42B19838614F772FeB5f42591",
+  ], // contributors
+  [60, 40], // shares
+];
