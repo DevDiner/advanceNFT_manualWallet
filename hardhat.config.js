c5f9abf10e4e1953f5a4b23491331c6c5e44d741
DevDiner <0xirwin@gmail.com>
2025-08-29 00:07:22 +0800

    added hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomicfoundation/hardhat-verify");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337, // Standard for local development
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
